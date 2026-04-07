f737e7be580d9027aea9ad1e90a41d47bc6c7e1b
ingbyr <zhangwenkai2@ccccltd.cn>
2026-04-07 14:40:36 +0800

    disable linux macos

--- script/publish.ts
+++ script/publish.ts
@@ -62,7 +62,7 @@ if (Script.release) {
     await $`git commit -am "release: v${Script.version}"`
     await $`git tag v${Script.version}`
     await $`git fetch origin`
-    await $`git cherry-pick HEAD..origin/dev`.nothrow()
+    await $`git cherry-pick HEAD..origin/lzdev`.nothrow()
     await $`git push origin HEAD --tags --no-verify --force-with-lease`
     await new Promise((resolve) => setTimeout(resolve, 5_000))
   }
